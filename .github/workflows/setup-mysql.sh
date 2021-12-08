f6df31dd858f8b9ab1effa48dc2a6e7f5029203c
Alexey Orlenko <alex@aqrln.net>
2021-12-08 16:41:08 +0200

    setup mysql on macos

---
+++ .github/workflows/setup-mysql.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+set -ex
+
+if [ "$RUNNER_OS" = "Windows" ]; then
+    # TODO
+fi
+
+if [ "$RUNNER_OS" = "macOS" ]; then
+    brew install mysql
+    brew services start mysql
+fi
+
+echo 'TEST_MYSQL_URI="mysql://root@localhost:3306/tests"' >> $GITHUB_ENV
+echo 'TEST_MYSQL_URI_MIGRATE="mysql://root@localhost:3306/tests-migrate"' >> $GITHUB_ENV
+echo 'TEST_MYSQL_SHADOWDB_URI_MIGRATE="mysql://root@localhost:3306/tests-migrate-shadowdb"' >> $GITHUB_ENV
