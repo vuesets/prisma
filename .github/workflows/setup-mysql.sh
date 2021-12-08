d595af433c62b8d5ea28f03cbc5e9d7ba701b6cb
Alexey Orlenko <alex@aqrln.net>
2021-12-08 17:46:18 +0200

    mysql connection string

--- .github/workflows/setup-mysql.sh
+++ .github/workflows/setup-mysql.sh
@@ -11,6 +11,6 @@ if [ "$RUNNER_OS" = "macOS" ]; then
     brew services start mysql
 fi
 
-echo 'TEST_MYSQL_URI="mysql://root@localhost:3306/tests"' >> $GITHUB_ENV
-echo 'TEST_MYSQL_URI_MIGRATE="mysql://root@localhost:3306/tests-migrate"' >> $GITHUB_ENV
-echo 'TEST_MYSQL_SHADOWDB_URI_MIGRATE="mysql://root@localhost:3306/tests-migrate-shadowdb"' >> $GITHUB_ENV
+echo 'TEST_MYSQL_URI="mysql://root:root@localhost:3306/tests"' >> $GITHUB_ENV
+echo 'TEST_MYSQL_URI_MIGRATE="mysql://root:root@localhost:3306/tests-migrate"' >> $GITHUB_ENV
+echo 'TEST_MYSQL_SHADOWDB_URI_MIGRATE="mysql://root:root@localhost:3306/tests-migrate-shadowdb"' >> $GITHUB_ENV
