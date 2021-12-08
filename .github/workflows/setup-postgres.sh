29a590ec553b38ad095b3425583124711bd84065
Alexey Orlenko <alex@aqrln.net>
2021-12-08 16:15:11 +0200

    improve postgres setup

--- .github/workflows/setup-postgres.sh
+++ .github/workflows/setup-postgres.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
+export PGDATA="$RUNNER_TEMP/pgdata"
+
 if [ "$RUNNER_OS" = "Windows" ]; then
-    echo "$PGBIN" >> $GITHUB_PATH
-    echo "PQ_LIB_DIR=$PGROOT\lib" >> $GITHUB_ENV
-
-    pg_ctl start
-
-    echo "TEST_POSTGRES_URI=postgres://postgres:root@localhost:5432/tests" >> $GITHUB_ENV
+    export PATH="$PGBIN:$PATH"
+    export PQ_LIB_DIR='$PGROOT\\lib'
+    export PGUSER="$USERNAME"
 fi
 
 if [ "$RUNNER_OS" = "macOS" ]; then
-    export PGDATA="$RUNNER_TEMP/pgdata"
-    echo "PGUSER=$USER" >> $GITHUB_ENV
-
-    pg_ctl init
-    pg_ctl start
-    createuser --createdb prisma
-    createdb -O prisma tests
-    psql -c "ALTER USER prisma PASSWORD 'prisma';" tests
-
-    echo "TEST_POSTGRES_URI=postgres://prisma:prisma@localhost:5432/tests" >> $GITHUB_ENV
+    export PGUSER="$USER"
 fi
+
+pg_ctl init
+pg_ctl start
+createuser --createdb prisma
+createdb -O prisma tests
+psql -c "ALTER USER prisma PASSWORD 'prisma';" tests
+
+echo "TEST_POSTGRES_URI=postgres://prisma:prisma@localhost:5432/tests" >> $GITHUB_ENV
